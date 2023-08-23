455ba8db1d9f61ab98801d39486d18d60af56155
Aaron Jencks <aaronjencks@aol.com>
2023-08-22 19:01:29 -0500

    added unit test for epsilon expansion

---
+++ creations/widget-programming/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEpsilonExpansion(t *testing.T) {
+	n := NewNfa()
+	n.transitions = map[int]map[rune][]int{
+		0: {
+			'.': {
+				1, 2, 3, 4,
+			},
+		},
+		1: {
+			'.': {
+				3, 5,
+			},
+			'a': {
+				6,
+			},
+		},
+		4: {
+			'.': {
+				7, 8,
+			},
+		},
+	}
+
+	neighbors := n.epsilonExpansion(0)
+
+	assert.Equal(t, []int{0, 1, 2, 3, 4, 7, 8, 5}, neighbors, "neighbor array mismatch")
+}
